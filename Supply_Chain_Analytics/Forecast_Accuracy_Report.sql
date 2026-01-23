dc1ff2f19c22b6c4e87bdba20c9d24d3d2afaa0f
Alisha Mahanty <alishamahanty0805@gmail.com>
2026-01-23 08:44:40 +0530

    Add files via upload

---
+++ Supply_Chain_Analytics/Forecast_Accuracy_Report.sql
@@ -0,0 +1,31 @@
+# forecast Accuracy Report
+
+WITH forecast_error_table AS (
+	SELECT 
+		s.customer_code AS customer_code,
+		c.customer AS customer_name,
+		c.market AS market,
+		SUM(s.sold_quantity) AS total_sold_qty,
+		SUM(s.forecast_quantity) AS total_forecast_qty,
+		SUM(s.forecast_quantity - s.sold_quantity) AS net_error,
+		ROUND(SUM(s.forecast_quantity - s.sold_quantity) * 100 / SUM(s.forecast_quantity),
+				1) AS net_error_pct,
+		SUM(ABS(s.forecast_quantity - s.sold_quantity)) AS abs_error,
+		ROUND(SUM(ABS(s.forecast_quantity - sold_quantity)) * 100 / SUM(s.forecast_quantity),
+				2) AS abs_error_pct
+	FROM
+		fact_act_est s
+			JOIN
+		dim_customer c ON s.customer_code = c.customer_code
+	WHERE
+		s.fiscal_year = 2021
+	GROUP BY customer_code
+)
+SELECT 
+    *,
+    IF(abs_error_pct > 100,
+        0,
+        100.0 - abs_error_pct) AS forecast_accuracy_pct
+FROM
+    forecast_error_table
+ORDER BY forecast_accuracy_pct DESC;
